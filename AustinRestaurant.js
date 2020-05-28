300c4616ee17d2270aba21ec8282df0199a32e9a
Diya <diya.rajon@yahoo.com>
2020-05-28 17:43:30 -0500

    Create AustinRestaurant.js

---
+++ AustinRestaurant.js
@@ -0,0 +1,22 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    for(var i = 0; i < myObj.restaurant.length;i++)
+    {
+      document.getElementById("demo").innerHTML = myObj.restaurant[i].name;
+    document.getElementById("demo").innerHTML +=" " + myObj.restaurant[i].dineIn;
+    var restaurantTitle = document.createElement("h1");
+    restaurantTitle.innerText = myObj.restaurant[i].name;
+    document.body.appendChild(restaurantTitle);
+    restaurantTitle.href = "something.com";
+    var para = document.createElement("P");
+    para.innerText = `takeout:${myObj.restaurant[i].takeout} gift card:${myObj.restaurant[i].giftCard}`;
+    document.body.appendChild(para);
+      }
+    
+  }
+};
+
+xmlhttp.open("GET", "side.json", true);
+xmlhttp.send();
