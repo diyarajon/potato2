76feddc5f4b789d995a6ace6627aca04a4e439fa
Diya <diya.rajon@yahoo.com>
2020-05-31 14:30:17 -0500

    Update AustinRestaurant.js

--- AustinRestaurant.js
+++ AustinRestaurant.js
@@ -4,22 +4,20 @@ xmlhttp.onreadystatechange = function() {
     var myObj = JSON.parse(this.responseText);
     for(var i = 0; i < myObj.restaurant.length;i++)
     {
-      document.getElementById("demo").innerHTML = myObj.restaurant[i].name;
-    document.getElementById("demo").innerHTML +=" " + myObj.restaurant[i].dineIn;
     var restaurantTitle = document.createElement("h1");
     restaurantTitle.id = 'someid';
     restaurantTitle.className = "hello";
     restaurantTitle.innerText = myObj.restaurant[i].name;
     document.body.appendChild(restaurantTitle);
-    //document.body.appendChild(restaurantTitle);
+    
     restaurantTitle.href = "index.html";
     var para = document.createElement("P");
     para.innerText = `takeout:${myObj.restaurant[i].takeout} gift card:${myObj.restaurant[i].giftCard} dine in:${myObj.restaurant[i].dineIn}`;
     document.body.appendChild(para);
+
     var a = document.createElement("a");
     var link = document.createTextNode("this is link");
     a.appendChild(link);
-    a.title = "this is link";
     a.href = "https://www.geeksforgeeks.org";
     document.body.appendChild(a);
 
