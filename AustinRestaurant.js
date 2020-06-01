abf572c0e600b6448105e48c63fad1c0d7cbe69d
Diya <diya.rajon@yahoo.com>
2020-06-01 02:52:42 -0500

    Update AustinRestaurant.js

--- AustinRestaurant.js
+++ AustinRestaurant.js
@@ -4,7 +4,7 @@ xmlhttp.onreadystatechange = function() {
     var myObj = JSON.parse(this.responseText);
     for(var i = 0; i < myObj.restaurant.length;i++)
     {
-    var something = "hello" + i;
+    var something = "hello" + myObj.restaurant[i].name;
     var restaurantTitle = document.createElement("h1");
     restaurantTitle.id = 'someid';
     restaurantTitle.className = something;
